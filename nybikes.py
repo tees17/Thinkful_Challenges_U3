69fd1e619819dab1ebef9854a948643656d35081
tees17 <teresa.o.sears@gmail.com>
2015-07-11 17:13:06 -0500

    deleting nybikes2

--- nybikes.py
+++ nybikes.py
@@ -10,46 +10,31 @@ df = json_normalize(r.json()['stationBeanList'])
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
 sql = "INSERT INTO citibike_reference (id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+station_ids = df['id'].tolist() 
+station_ids = ['_' + str(x) + ' INT' for x in station_ids]
 
 with con:
     cur.execute('CREATE TABLE citibike_reference (id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT )')
-
-
-
-#for loop to populate values in the database
-with con:
     for station in r.json()['stationBeanList']:
         #id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location)
         cur.execute(sql,(station['id'],station['totalDocks'],station['city'],station['altitude'],station['stAddress2'],station['longitude'],station['postalCode'],station['testStation'],station['stAddress1'],station['stationName'],station['landMark'],station['latitude'],station['location']))
 
-        #extract the column from the DataFrame and put them into a list
-
-station_ids = df['id'].tolist() 
-
-#add the '_' to the station name and also add the data type for SQLite
-station_ids = ['_' + str(x) + ' INT' for x in station_ids]
-
-#create the table
-#in this case, we're concatentating the string and joining all the station ids (now with '_' and 'INT' added)
-with con:
     cur.execute('CREATE TABLE available_bikes (execution_time INT, ' +  ', '.join(station_ids) + ');')
 
-
-exec_time = parse(r.json()['executionTime'])
-
-with con:
+for i in range (60):
+    r = requests.get('http://www.citibikenyc.com/stations/json')
+    exec_time = parse(r.json()['executionTime'])
     cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%Y-%m-%d %H:%M:%S'),))
+    id_bikes = collections.defaultdict(int) 
 
+    #loop through the stations in the station list
+    for station in r.json()['stationBeanList']:
+        id_bikes[station['id']] = station['availableBikes']
+        con.commit()
 
-#defaultdict to store available bikes by station
-id_bikes = collections.defaultdict(int) 
-
-#loop through the stations in the station list
-for station in r.json()['stationBeanList']:
-    id_bikes[station['id']] = station['availableBikes']
-
-#iterate through the defaultdict to update the values in the database
-with con:
+    #iterate through the defaultdict to update the values in the database
 	for k, v in id_bikes.iteritems():
 		cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = '" + exec_time.strftime('%Y-%m-%d %H:%M:%S') + "';")
-con.commit()		
+        con.commit()
+    time.sleep(60)
+con.close()		
